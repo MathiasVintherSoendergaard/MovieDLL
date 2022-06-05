ff2e7956bc4d574990060ff53900a403391fb0d6
Mathias Vinther Søndergaard <mvso32774@edu.ucl.dk>
2022-06-05 12:39:04 +0200

    Add project files.

---
+++ MovieDLL/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MovieDLL
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int LengthInMinutes { get; set; }
+        public int ConvertLengthInMinutesToSeconds()
+        {
+            if (LengthInMinutes == 0 || LengthInMinutes < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return LengthInMinutes * 60;
+        }
+    }
+}
---
+++ MovieDLLTests/MovieTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDLL.Tests
+{
+    [TestClass()]
+    public class MovieTests
+    {
+        Movie newMovie = new Movie() { Id = 1, Name = "Reservoir Dogs", Country = "America", LengthInMinutes = 99 };
+        [TestMethod()]
+        public void ConvertLengthInMinutesToSecondsTestValid()
+        {
+            Assert.AreEqual(5940, newMovie.ConvertLengthInMinutesToSeconds());
+        }
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            Assert.AreEqual(1, newMovie.Id);
+            Assert.AreEqual("Reservoir Dogs", newMovie.Name);
+            Assert.AreEqual("America", newMovie.Country);
+            Assert.AreEqual(99, newMovie.LengthInMinutes);
+        }
+        [TestMethod()]
+        public void ConvertLengthInMinutesToSecondsTestInvalid()
+        {
+            newMovie.LengthInMinutes = 0;
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => newMovie.ConvertLengthInMinutesToSeconds());
+            newMovie.LengthInMinutes = -1;
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => newMovie.ConvertLengthInMinutesToSeconds());
+        }
+    }
+}
